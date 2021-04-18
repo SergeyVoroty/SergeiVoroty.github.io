b71e85fe13188c5d2f53bbfb950002ab55a7f712
Sergey_Voroty <sergei.vorot22@gmail.com>
2021-04-19 00:01:03 +0300

    Added mixin for Email-form and Buttons

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,10 @@ const HTMLWebpackPlugin = require('html-webpack-plugin')
 const pug = {
     test: /\.pug$/,
     use: ['html-loader', 'pug-html-loader']
-};
+}
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
 module.exports = {
+    context: path.resolve(__dirname),
     mode: "development",
     entry: './src/app.js',
     output: {
@@ -15,15 +17,23 @@ module.exports = {
         rules: [
             pug,
             {
-                test: /\.css$/,
+                test: /\.(sass|css|scss)$/,
                 use: [
                     'style-loader',
-                    'css-loader'
+                    'css-loader',
+                    'sass-loader'
                 ]
-            }
+            },
+            {
+                test: /\.(png|jpg|svg|gif|ttf|woff)$/,
+                use: [
+                    'file-loader'
+                ]
+            },
         ]
     },
     plugins: [
-        new HTMLWebpackPlugin({template: "./src/index.pug"})
+        new HTMLWebpackPlugin({template: "./src/index.pug", minify: false}),
+        new CleanWebpackPlugin()
     ]
 }
